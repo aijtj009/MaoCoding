b27b4d11f0f954355ec29ed5129b762b3eb9d818
aijtj009 <aijtj009@163.com>
2018-03-14 23:46:48 +1100

    d

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
-for i in range(9):
-    print(i)
-    for j in range(10):
-        print(j)
-    print(i,j)
+n=input("Type in Password\n")
+if 'mao' == n:
+    for i in range(9):
+        print(i)
+        for j in range(10):
+            print(j)
+        print(i,j)
+else:
+    print("Wrong Password")
+    
+# 首先是F1/Ctrl+Shit+P萬能鍵，誰用誰知道
+# Ctrl+P：文檔切換
+# Ctrl+空格：自動提示
+# F12/Ctrl+左鍵：跳轉到定義
+# Shift+F12：預覽定義
+# Ctrl+G：跳轉行號
+# Ctrl+/：註釋切換
+# Alt+↑↓：整行上下移動
+# Ctrl+↑↓：編輯器垂直滾動條上下移動，光標不動
+# Ctrl+Backspace/Delete：整詞/連續空白刪除
+# Ctrl+→←：光標整詞移動
+# Ctrl+F查找/Ctrl+Shift+F在文檔中查找，這都屬於通用的，類似的就不説了啊。 
+
+
+# 隨時隨地，F5調試運行。注意，VSCode默認在調試後，會停在第一句，如下圖。
+
+# 　　調試快捷鍵與VS基本相同。
+
+# F5：調試/繼續，
+# F10：單步跳過，
+# F11：單步進入，
+# Shift+F11：跳出。
+# F9：切換斷點
